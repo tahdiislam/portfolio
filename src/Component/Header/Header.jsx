f6b1faa108a0e3c9da414351b779039d9b42c315
tahdi-islam <tshdi60@gmail.com>
2022-12-10 04:31:17 +0600

    add link tag

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -1,23 +1,32 @@
-import React from 'react';
-import photo from "../../assets/Tahdi-Islam.jpg"
+import React from "react";
+import { Link } from "react-router-dom";
+import photo from "../../assets/Tahdi-Islam.jpg";
 
 const Header = () => {
-    return (
-        <nav className='flex items-center justify-between mx-16 py-4 text-xl font-semibold'>
-            <div className='flex w-1/4 items-center'>
-                <img className='h-16 w-16 rounded-full' src={photo} alt="" />
-                <h2 className='ml-4'>TAHDI ISLAM</h2>
-            </div>
-            <div>
-                <ul className='flex ml-6'>
-                    <li className='ml-12'>HOME</li>
-                    <li className='ml-12'>ABOUT</li>
-                    <li className='ml-12'>PROJECT</li>
-                    <li className='ml-12'>CONTACT</li>
-                </ul>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="flex items-center justify-between px-16 py-4 text-xl font-semibold shadow-lg">
+      <div className="flex w-1/4 items-center">
+        <img className="h-16 w-16 rounded-full" src={photo} alt="" />
+        <h2 className="ml-4">TAHDI ISLAM</h2>
+      </div>
+      <div>
+        <ul className="md:flex ml-6">
+          <li className="ml-12">
+            <a href="/">HOME</a>
+          </li>
+          <li className="ml-12">
+            <a href="/">ABOUT</a>
+          </li>
+          <li className="ml-12">
+            <a href="/">PROJECT</a>
+          </li>
+          <li className="ml-12">
+            <a href="/">CONTACT</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
