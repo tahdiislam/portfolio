5687f330b142073b557125c1d844ff4eeb5a0dcc
tahdi-islam <tshdi60@gmail.com>
2022-12-10 07:28:14 +0600

    add contact form

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import About from "./Component/About/About"
-import Header from "./Component/Header/Header"
-import Home from "./Component/Home/Home"
-import Projects from "./Component/Projects/Projects"
+import About from "./Component/About/About";
+import Contact from "./Component/Contact/Contact";
+import Header from "./Component/Header/Header";
+import Home from "./Component/Home/Home";
+import Projects from "./Component/Projects/Projects";
 
 function App() {
   return (
     <div>
-      <Header/>
-      <Home/>
-      <About/>
-      <Projects/>
+      <Header />
+      <Home />
+      <About />
+      <Projects />
+      <Contact />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Component/Contact/Contact.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+
+const Contact = () => {
+  // form submit handler
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div>
+      <div className="w-1/2 mx-auto mb-10">
+        <h2 className="text-3xl font-bold text-center mb-8">CONTACT</h2>
+        <p className="text-xl text-center">
+          Feel free to Contact me by submitting the form below and I will get
+          back to you as soon as possible
+        </p>
+      </div>
+      <div class="flex items-center justify-start bg-white">
+        <div class="mx-auto w-full max-w-lg">
+          <form onSubmit={handleFormSubmit} class="mt-10">
+            <input
+              type="hidden"
+              name="access_key"
+              value="YOUR_ACCESS_KEY_HERE"
+            />
+            <div class="grid gap-6 sm:grid-cols-2">
+              <div class="relative z-0">
+                <input
+                  type="text"
+                  name="name"
+                  class="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0"
+                  placeholder=" "
+                />
+                <label class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">
+                  Your Name
+                </label>
+              </div>
+              <div class="relative z-0">
+                <input
+                  type="email"
+                  name="email"
+                  class="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0"
+                  placeholder=" "
+                />
+                <label class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">
+                  Your Email
+                </label>
+              </div>
+              <div class="relative z-0 col-span-2">
+                <textarea
+                  name="message"
+                  rows="5"
+                  class="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0"
+                  placeholder=" "
+                ></textarea>
+                <label class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">
+                  Your Message
+                </label>
+              </div>
+            </div>
+            <button
+              type="submit"
+              class="mt-5 rounded-md bg-green-400 hover:bg-green-500 px-10 py-2 text-white"
+            >
+              Send Message
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
