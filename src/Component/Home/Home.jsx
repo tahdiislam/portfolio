2c6966c639267f28e3299f02b28c25277765bb8c
tahdi-islam <tshdi60@gmail.com>
2022-12-10 08:34:51 +0600

    responsive home section

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -4,12 +4,13 @@ import Button from "../Button/Button";
 const Home = () => {
   return (
     <div
-      className="bg-white"
+      className="bg-white py-16 md:py-48"
       style={{
         backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8' viewBox='0 0 8 8'%3E%3Cg fill='%2380877f' fill-opacity='0.11'%3E%3Cpath fill-rule='evenodd' d='M0 0h4v4H0V0zm4 4h4v4H4V4z'/%3E%3C/g%3E%3C/svg%3E")`,
+        backgroundSize: "auto"
       }}
     >
-      <div className="w-1/2 mx-auto my-48 flex flex-col items-center">
+      <div className="w-3/4 md:w-1/2 mx-auto flex flex-col items-center">
         <h1 className="text-5xl font-bold mb-10">HEY, I'M TAHDI ISLAM</h1>
         <p className="text-xl mb-10 text-center">
           A Frontend focused Web Developer building the Frontend of Websites and
