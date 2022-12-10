96dd038d7733317a3017af1b0420cad13c86e021
tahdi-islam <tshdi60@gmail.com>
2022-12-10 20:28:07 +0600

    add projects

--- src/Component/Projects/Projects.jsx
+++ src/Component/Projects/Projects.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import img01 from "../../assets/projectImg/project-01.png";
+import img02 from "../../assets/projectImg/project-02.png";
+import img03 from "../../assets/projectImg/project-03.png";
 import Button from "../Button/Button";
 
 const projects = [
@@ -7,19 +9,22 @@ const projects = [
     img: img01,
     name: "Laptop Mart",
     details:
-      "Laptop Mart is a platform that I created where people can join the community of their favorite youtube channels and can be part of the conversation.",
+      "Laptop Mart is a multi vendor e-commerce where I seller can sell old laptop, a buyer can buy laptop, and the Admin can manage his user and all product.",
+    link: "https://laptop-mart-26.web.app/",
   },
   {
-    img: img01,
-    name: "Laptop Mart",
+    img: img02,
+    name: "Snack Deck",
     details:
-      "Laptop Mart is a platform that I created where people can join the community of their favorite youtube channels and can be part of the conversation.",
+      "Snack deck is simple personal cloud kitchen where the seller can sell his food and the user can buy any food and review in food, also can manage his review",
+    link: "https://snackdeck-a569c.web.app/",
   },
   {
-    img: img01,
-    name: "Laptop Mart",
+    img: img03,
+    name: "Code Tutor",
     details:
-      "Laptop Mart is a platform that I created where people can join the community of their favorite youtube channels and can be part of the conversation.",
+      "Code Tutor is a AdTech platform where the seller can sell their courses. and the user can buy any kind of curses",
+    link: "https://code-tutor-255e9.web.app/",
   },
 ];
 
@@ -43,7 +48,11 @@ const Projects = () => {
             <div className="md:w-1/2 md:p-16">
               <h2 className="text-3xl font-bold mb-10">{project.name}</h2>
               <p className="text-xl mb-10">{project.details}</p>
-              <Button style="py-2 px-6 text-lg md:py-3 md:px-8 md:text-xl">PREVIEW</Button>
+              <a target="_blank" href={project.link}>
+                <Button style="py-2 px-6 text-lg md:py-3 md:px-8 md:text-xl">
+                  PREVIEW
+                </Button>
+              </a>
             </div>
           </div>
         ))}
